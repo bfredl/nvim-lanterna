ecbb0ba85bd8fc3dcfdb727c522673036351057b
bfredl <bjorn.linse@gmail.com>
2022-12-24 21:46:46 +0100

    shell the term

---
+++ lua/lanterna/term_shell.lua
@@ -0,0 +1,65 @@
+local h = _G._lanterna_term_state or {}
+_G._lanterna_term_state = h
+_G.h = h
+
+local a = vim.api
+
+h.handlers = h.handlers or {}
+local handlers = h.handlers
+
+function h.ensure_term()
+  if h.buf == nil then
+    h.buf = a.nvim_create_buf(false, true)
+
+    local w0 = a.nvim_get_current_win()
+    a.nvim_command("split")
+    local w = a.nvim_get_current_win()
+    a.nvim_win_set_buf(w,h.buf)
+    a.nvim_set_current_win(w0)
+
+    h.term = a.nvim_open_term(h.buf, {})
+    a.nvim_buf_set_name(h.buf, "[lanterna-iopub]")
+  end
+end
+
+function h.send(data)
+  a.nvim_chan_send(h.term, data)
+end
+
+function h.sendlines(data)
+  a.nvim_chan_send(h.term, (string.gsub(data, '\n', '\r\n')))
+end
+
+function handlers.execute_input(c)
+  h.sendlines("In  ["..c.execution_count.."]: "..c.code..'\n')
+end
+
+function handlers.execute_result(c)
+  local datas = c.data["text/plain"]
+  if datas then
+    h.sendlines("Out ["..c.execution_count.."]: "..datas..'\n')
+  end
+end
+
+function handlers.status(c)
+  h.state = c.execution_state
+  -- p("state: ",c.execution_state)
+end
+
+
+function h.register(client)
+  h.ensure_term()
+  local luadev = require'luadev'
+
+  client:poll_iopub(luadev.schedule_wrap(function(mess)
+    local hnd = handlers[mess.header.msg_type]
+    if hnd then
+      hnd(mess.content)
+    else
+      local p = luadev.print
+      p('FEL:', mess.header.msg_type)
+      p(vim.inspect(mess.content))
+    end
+  end))
+end
+return h
--- scratch.lua
+++ scratch.lua
@@ -1,12 +1,22 @@
-
 client = require'lanterna'.connect_latest()
+term_shell = require'lanterna.term_shell'
+term_shell.sendlines('moron\n')
 
-client:iopub_handlers()
+term_shell.register(client)
 client:poll_shell()
+vim.pretty_print(client.config)
+
+function rq(code)
+  client:shell_request('execute_request', {code=code, silent=false}, function()
+  end)
+end
 
 msg = client:shell_request('execute_request', {code='30**3', silent=false}, function(aaaa) 
   vim.schedule(function() require'luadev'.print("nanana") end)
 end)
+
+rq 'errrrrrrorr'
+rq '1+2'
 
 datta = client.shell:recv_all()
 reply = client.session:decode(datta)
